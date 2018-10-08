6053edbee526e77d849ffb83135357aad7ce4d41
Lavrentijs L <llembersk@gmail.com>
2018-10-09 00:40:44 +0300

    added buttons

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
-import React, { Component } from 'react';
-import Table from './Table';
-import Form from './Form';
+import React, { Component } from "react";
+import Table from "./Table";
+import Form from "./Form";
 
 class App extends Component {
-    state = {
-        characters: []
-    };
+  state = {
+    characters: []
+  };
 
-    removeCharacter = index => {
-        const { characters } = this.state;
-    
-        this.setState({
-            characters: characters.filter((character, i) => { 
-                return i !== index;
-            })
-        });
-    }
+  removeCharacter = index => {
+    const { characters } = this.state;
 
-    handleSubmit = character => {
-        this.setState({characters: [...this.state.characters, character]});
-    }
+    this.setState({
+      characters: characters.filter((character, i) => {
+        return i !== index;
+      })
+    });
+  };
 
-    render() {
-        const { characters } = this.state;
-        
-        return (
-            <div className="container">
-                <h1>React Tutorial</h1>
-                <p>Add a character with a name and a job to the table.</p>
-                <Table
-                    characterData={characters}
-                    removeCharacter={this.removeCharacter}
-                />
-                <Form handleSubmit={this.handleSubmit} />
-            </div>
-        );
-    }
+  handleSubmit = character => {
+    this.setState({ characters: [...this.state.characters, character] });
+  };
+
+  render() {
+    const { characters } = this.state;
+
+    return (
+      <div className="container">
+        <h3>TODO List improved</h3>
+        <button>ALL</button>
+        <button>ACTIVE</button>
+        <button>COMPLETE</button>
+        <div>
+          <button>LEFT </button>
+        </div>
+        <h1>React Tutorial</h1>
+        <Table
+          characterData={characters}
+          removeCharacter={this.removeCharacter}
+        />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App;
